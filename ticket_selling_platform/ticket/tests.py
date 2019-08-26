d70d987a5719f8606256fe432d07a7813c3f3adf
IKarbowiak <idzi.karbowiak@gmail.com>
2019-08-26 18:19:52 +0200

    Add test for ticket model

--- ticket_selling_platform/ticket/tests.py
+++ ticket_selling_platform/ticket/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import TicketType
+from event.models import Event
+
+
+class TicketTypeModelTest(TestCase):
+
+    def test_create_tickets(self):
+        # GIVEN
+        ticket_type = TicketType.objects.create(price=10, type=TicketType.REGULAR)
+        event = Event.objects.create(name='TestEvent', description='TestDescription',
+                                     datetime=timezone.now() + timedelta(days=2))
+
+        # WHEN
+        ticket_type.create_tickets(event, 10)
+
+        # THEN
+        self.assertEqual(event.tickets.all().count(), 10)
